d5b6b459a6ae93573fc34c8c1b8f8427066cff7a
wirahmat <58240412+wirahmat@users.noreply.github.com>
2022-05-26 19:00:04 +0700

    Big Update
    create flask, moving the images folder to static, integrating with choropleth map

---
+++ app.py
@@ -0,0 +1,40 @@
+"""This is a simple flask web server, meant only to return index.html"""
+
+from tempfile import mkdtemp
+from flask import Flask, render_template
+from flask_session import Session
+from flask_cors import CORS
+from werkzeug.exceptions import default_exceptions
+
+# Initializing flask application
+app = Flask(__name__)
+cors = CORS(app)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] is True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem instead of cookies
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Routes
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    """Index route main function"""
+
+    return render_template('index.html')
+
+for code in default_exceptions:
+    app.errorhandler(code)
+    
